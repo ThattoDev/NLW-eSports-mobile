89780e3905902c8c38f8ff97752538b45999ba3f
ThattoDev <thacito.pfilgueiras@gmail.com>
2023-01-27 17:09:15 -0300

    :fire: adição do tailwind

--- App.tsx
+++ App.tsx
@@ -1,49 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Background } from './src/components/Background';
+import { StatusBar } from "react-native";
+import { useFonts,
+         Inter_400Regular,
+         Inter_600SemiBold,
+         Inter_700Bold,
+         Inter_900Black
+} from "@expo-google-fonts/inter";
+import { Home } from "./src/screens/Home";
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native</Text>
-      <StatusBar style="auto" backgroundColor="lightblue"/>
-      <Button title='send 1' />
-      <Button title='send 2' />
-      <Button title='send 3' />
-    </View>
+    <Background >
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      {fontsLoaded ? <Home/> : <Loading/> }
+    </Background>
   );
 }
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text style={styles.button}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#A12',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  button: {
-    backgroundColor: '#Bd1',
-    width: 50,
-    height: 20,
-    marginBottom: 10,
-  }
-});
